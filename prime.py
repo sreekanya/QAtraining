922ee14400b1f820ba2629c82595bc05db75c61c
sreekanya <sreekanya@gmail.com>
2014-01-27 17:55:43 -0800

    Prime number printing

---
+++ prime.py
@@ -0,0 +1,9 @@
+#! /usr/bin/python
+
+a=range(4,100)
+print "1 is a prime number"
+print "2 is a prime number"
+print "3 is a prime number"
+for i in a:
+	if((i%2!=0)and(i%3!=0)and(i%5!=0)and(i%7!=0)):
+	  print i," is prime number"
